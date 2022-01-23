153a9a619443547dd0be429696fef175e4419d5a
Sean M <smidgley792@gmail.com>
2022-01-23 10:00:22 -0800

    button scroll concept

--- Components/Form.js
+++ Components/Form.js
@@ -2,17 +2,17 @@ import HoverLink from "./HoverLink";
 
 export default function Form() {
 	return (
-		<div className="max-w-prose mx-auto bg-stone-200 p-6 shadow-lg shadow-black ">
+		<div className="max-w-prose mx-auto bg-stone-200 p-6 shadow-lg shadow-black text-black rounded-lg">
 			<form className="flex flex-col w-full">
 				<label className="mt-4">
 					<span className="text-stone-800 font-bold mb-2 block text-sm">Name</span>
-					<input type="text" name="name" className="w-full p-4" />
+					<input type="text" name="name" className="w-full p-4" placeholder="Enter Name" />
 				</label>
 				<label className="mt-4 block w-full"><span className="text-stone-800 font-bold mb-2 block text-sm">Email</span>
-					<input type="text" name="email" className="w-full p-4" />
+					<input type="text" name="email" className="w-full p-4" placeholder="Enter Email" />
 				</label>
 				<label className="mt-4"><span className="text-stone-800 font-bold mb-2 block text-sm">Message</span>
-					<textarea type="text" name="name" className="w-full p-4 h-36" />
+					<textarea type="text" name="name" className="w-full p-4 h-36" placeholder="Enter Message" />
 				</label>
                 <HoverLink className="text-center mt-6">Submit</HoverLink>
 			</form>
--- Components/HoverLink.js
+++ Components/HoverLink.js
@@ -3,7 +3,7 @@ export default function HoverLink({ children, href, bgColor="bg-black", textColo
 	return (
 		<a
 			href={href}
-			className={` ${bgColor} ${defaultClass} ${textColor} py-2 px-8 rounded-xl font-bold duration-300 transition-transform ease-out hover:-translate-y-2`}
+			 className={` ${bgColor} ${defaultClass} ${textColor} py-2 px-8 rounded-xl font-bold duration-300 transition-transform ease-out hover:-translate-y-2`}
 		>
 			{children}
 		</a>
