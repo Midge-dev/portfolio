81cfcd3c00674dc1e4332e9bd56b7886b177da8d
Sean M <smidgley792@gmail.com>
2022-03-05 16:13:23 -0800

    almost there

--- Components/Project.js
+++ Components/Project.js
@@ -5,8 +5,11 @@ import Link from 'next/link';
 export default function Project({ url, fallbackImageSrc, title, description, caseStudyRoute }) {
 	return (
 		<div className="flex flex-col md:flex-row gap-4 w-auto h-auto drop-shadow-md ">
-			<LinkPreview url={url} width="320px" fallbackImageSrc={fallbackImageSrc} />
-			<div className='max-w-[65vw]'>
+			<div className='w-1/3 h-auto'>
+			<LinkPreview url={url} width="100%" fallbackImageSrc={fallbackImageSrc} />
+			</div>
+			
+			<div className='w-2/3'>
 				<h3 className="text-xl font-bold">{title}</h3>
 				<p className='py-6'>{description}</p>
 				<div className="flex justify-end p-4">
