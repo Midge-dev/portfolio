41c169d8b443ddc9bf22b030e08210ba16abdc9d
Sean M <smidgley792@gmail.com>
2022-03-05 16:57:31 -0800

    more tweaks

--- Components/Project.js
+++ Components/Project.js
@@ -4,17 +4,17 @@ import HoverLink from './HoverLink';
 import Link from 'next/link';
 export default function Project({ url, fallbackImageSrc, title, description, caseStudyRoute }) {
 	return (
-		<div className="flex flex-col md:flex-row gap-4 w-auto h-auto drop-shadow-md ">
-			<div className='w-1/3 h-auto'>
+		<div className="flex justify-center flex-col md:flex-row gap-4 w-auto h-auto drop-shadow-md ">
+			<div className='w-[320px] sm:w-1/3 h-auto mx-auto'>
 			<LinkPreview url={url} width="100%" fallbackImageSrc={fallbackImageSrc} />
 			</div>
 			
 			<div className='w-2/3'>
 				<h3 className="text-xl font-bold">{title}</h3>
-				<p className='py-6'>{description}</p>
-				<div className="flex justify-end p-4">
+				<p className='py-6 text-center'>{description}</p>
+				<div className="flex justify-end sm:justify-center p-4">
 					<Link passHref href={caseStudyRoute}>
-					<HoverLink type='Link' url={caseStudyRoute} className=''>Case Study</HoverLink>
+					<HoverLink type='Link' url={caseStudyRoute} className='sm:flex justify-center'>Case Study</HoverLink>
 					</Link>
 				</div>
 			</div>
